a2168464e2f3bce3c130346db8a5df54377390fd
Robert Schäfer <git@roschaefer.de>
2026-03-17 23:13:01 +0100

    refactor: include my name in filename (#3)

--- src/lib/data/resume-pdf.ts
+++ src/lib/data/resume-pdf.ts
@@ -1,3 +1,5 @@
-export const resumePdfPath = (locale: "de" | "en"): string => `/resume.${locale}.pdf`;
+export const resumePdfFilename = (locale: "de" | "en"): string =>
+	`robert-schaefer-resume.${locale}.pdf`;
 
-export const resumePdfFilename = (locale: "de" | "en"): string => `resume.${locale}.pdf`;
+export const resumePdfPath = (locale: "de" | "en"): string =>
+	`/${locale}/${resumePdfFilename(locale)}`;
